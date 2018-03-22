d60d232d46ec6e391d38f8bdee5c68f9e90edd49
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-22 16:24:22 -0400

    Getting ids

--- Client/src/main/java/YouAreEll.java
+++ Client/src/main/java/YouAreEll.java
@@ -1,12 +1,33 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.*;
+
+import java.io.IOException;
+
 public class YouAreEll {
 
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private final String baseUrl = "http://zipcode.rocks:8085";
+    private OkHttpClient client;
+    private ObjectMapper mapper;
+    private String fromId;
+
     YouAreEll() {
+        client = new OkHttpClient();
+        mapper = new ObjectMapper();
+        fromId = "someone";
     }
 
     public static void main(String[] args) {
-        YouAreEll urlhandler = new YouAreEll();
-        System.out.println(urlhandler.MakeURLCall("/ids", "GET", ""));
-        System.out.println(urlhandler.MakeURLCall("/messages", "GET", ""));
+        YouAreEll urlHandler = new YouAreEll();
+        ObjectMapper mapper = new ObjectMapper();
+    }
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
     }
 
     public String get_ids() {
@@ -17,7 +38,41 @@ public class YouAreEll {
         return MakeURLCall("/messages", "GET", "");
     }
 
-    public String MakeURLCall(String mainurl, String method, String jpayload) {
-        return "nada";
+    public String MakeURLCall(String mainUrl, String method, String jpayload) {
+        String fullUrl = baseUrl + mainUrl;
+        Request request = null;
+
+        if (method.equalsIgnoreCase("get")) {
+            request = new Request.Builder()
+                    .url(fullUrl)
+                    .build();
+        }
+
+
+        if (method.equalsIgnoreCase("POST")) {
+            RequestBody body = RequestBody.create(JSON, jpayload);
+            request = new Request.Builder()
+                    .url(fullUrl)
+                    .post(body)
+                    .build();
+        }
+
+        if (method.equalsIgnoreCase("PUT")) {
+            RequestBody body = RequestBody.create(JSON, jpayload);
+            request = new Request.Builder()
+                    .url(fullUrl)
+                    .put(body)
+                    .build();
+        }
+
+        if (request != null) {
+            try (Response response = client.newCall(request).execute()) {
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "error: bad method " + method;
     }
 }
